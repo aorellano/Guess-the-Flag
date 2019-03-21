580e6e0ef797e8b852c57baeb7f2b59593fc00e0
Alexis Orellano <alexisorellano@Andrews-MacBook-Pro-2.local>
2019-03-21 17:35:21 -0500

    ActivityViewController added

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    var questionCtr = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        askQuestion(action: nil)
     }
-
-
+    func askQuestion(action: UIAlertAction!){
+        countries.shuffle()
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        correctAnswer = Int.random(in: 0...2)
+        title = "\(countries[correctAnswer].uppercased())                      Score: \(score)"
+        if questionCtr == 10{
+            let ac = UIAlertController(title: "Final Score", message: "\(score) out of 10", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .cancel))
+            present(ac, animated: true)
+        }
+        questionCtr += 1
+    }
+    @IBAction func buttonTapped(_ sender: Any) {
+        var title: String
+        var message: String
+        if (sender as AnyObject).tag == correctAnswer {
+            score += 1
+            title = "Correct"
+            message = "Your score is \(score)"
+        } else {
+            title = "Wrong!"
+            message = "That is the flag of \(countries[(sender as AnyObject).tag].uppercased())"
+        }
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
 }
 
